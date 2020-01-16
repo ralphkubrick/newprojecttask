bed6007c078623346616e24271bc682156874795
ralphkubrick <59834908+ralphkubrick@users.noreply.github.com>
2020-01-16 14:00:01 +0000

    create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+from urllib import request
+
+url = r'https://www.bbc.co.uk/news'
+req = requests.Request(url)
+
+with request.urlopen(req) as r:
+    soup = r.read()
+
+title = soup.title.string
+print(title)
